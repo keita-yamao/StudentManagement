cdd9fbdaee8e9ed4348fe7c0626a818bb7afa386
Kaita Yamao <s3991t1222@outlook.jp>
2024-10-07 02:21:13 +0900

    ターミナルを使ったGetMappingとPostMappingのコード作成

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,28 +1,53 @@
 package raisetech.StudentManagement;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-  String word = "RaiseTech";
+  public String name = "Keita Yamao";
+  public String age = "34";
+  private Map<String, String> student = new HashMap<>();
 
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/judgeWord")
-  public String getJudgeWord() {
-    if (StringUtils.isEmpty(word)) {
-      return "空白の文字列です";
-    } else {
-      return word;
-    }
+  //nameとageをget
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    return name + " " + age + "歳";
   }
 
+  //Mapをget
+  @GetMapping("/studentMap")
+  public Map<String, String> getStudentMap() {
+    return student;
+  }
+
+  //nameとageにPost
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  //nameにPost
+  @PostMapping("/studentName")
+  public void updateStudentName(String name) {
+    this.name = name;
+  }
+
+  //MapにPost
+  @PostMapping("/studentMap")
+  public void addStudentMap(String name,String age) {
+    student.put(name,age);
+  }
 }
