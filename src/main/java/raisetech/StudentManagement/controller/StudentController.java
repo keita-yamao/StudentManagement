b81fdc18a6c72746813a9337081aa60ef7417538
Kaita Yamao <s3991t1222@outlook.jp>
2024-11-18 20:21:57 +0900

    コース情報のリスト表示のメソッドにおいて、パスパラメーターで指定するように変更

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -3,6 +3,7 @@ package raisetech.StudentManagement.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.data.Student;
@@ -26,8 +27,8 @@ public class StudentController {
   }
 
   //コース情報のリスト表示
-  @GetMapping("/courseList")
-  public List<studentsCourses> getCourseList(@RequestParam String courseId) {
+  @GetMapping("/courseList/{courseId}")
+  public List<studentsCourses> getCourseList(@PathVariable String courseId) {
     return service.searchCourseList(courseId);
   }
 }
