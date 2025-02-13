98d38dd2626d1605ffef40b5244bfe6e0da84929
Kaita Yamao <s3991t1222@outlook.jp>
2025-02-13 23:56:27 +0900

    StudentConverterの記述をStreamAPIを用いた記述に変更

--- src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
+++ src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
@@ -2,6 +2,7 @@ package raisetech.StudentManagement.controller.converter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import raisetech.StudentManagement.data.Course;
 import raisetech.StudentManagement.data.Student;
@@ -17,25 +18,22 @@ public class StudentConverter {
     //生徒情報＋リスト(受講情報+コース情報)を持つreturn用リストオブジェクト作成
     List<StudentDetail> studentDetails = new ArrayList<>();
     //生徒情報ごとに処理
-    for (Student student : students) {
+    students.forEach(student -> {
       //生徒情報＋リスト(受講情報+コース情報)オブジェクト作成
       StudentDetail studentDetail = new StudentDetail();
       //リスト(受講情報+コース情報)オブジェクト作成
-      List<CourseDetail> courseDetailList = new ArrayList<>();
-      //生徒情報とリスト(受講情報+コース情報)の突合処理
-      for (CourseDetail courseDetail : courseDetails) {
-        //生徒IDが一致するもの検索
-        if (student.getStudentId().equals(courseDetail.getStudentsCourses().getStudentId())) {
-          courseDetailList.add(courseDetail);
-        }
-      }
+      List<CourseDetail> courseDetailList = courseDetails.stream().filter(
+              //生徒IDをもとに生徒情報とリスト(受講情報+コース情報)の突合処理
+              courseDetail -> student.getStudentId()
+                  .equals(courseDetail.getStudentsCourses().getStudentId()))
+          .collect(Collectors.toList());
       //生徒情報＋受講情報+コース情報をset
       if (!courseDetailList.isEmpty()) {
         studentDetail.setStudent(student);
         studentDetail.setCourseDetail(courseDetailList);
         studentDetails.add(studentDetail);
       }
-    }
+    });
     return studentDetails;
   }
 
@@ -44,19 +42,18 @@ public class StudentConverter {
     //受講情報＋コース情報のリストの作成
     List<CourseDetail> courseDetails = new ArrayList<>();
     //受講情報ごとに処理
-    for (StudentsCourses studentsCourse : studentsCourses) {
+    studentsCourses.forEach(studentsCourse -> {
       //受講情報＋コース情報のオブジェクト作成
       CourseDetail courseDetail = new CourseDetail();
-      for (Course course : courses) {
-        //受講情報とコース情報の突合処理
-        if (studentsCourse.getCourseId().equals(course.getCourseId())) {
-          courseDetail.setCourse(course);
-          courseDetail.setStudentsCourses(studentsCourse);
-        }
-      }
+      //受講情報とコース情報の突合処理
+      courses.stream().filter(course -> studentsCourse.getCourseId().equals(course.getCourseId()))
+          .forEach(course -> {
+            courseDetail.setCourse(course);
+            courseDetail.setStudentsCourses(studentsCourse);
+          });
       //リスト(受講情報+コース情報)に追加
       courseDetails.add(courseDetail);
-    }
+    });
     return courseDetails;
   }
 }
